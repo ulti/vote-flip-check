1cfe9b432269996f91b08eda5043a83676c8051f
Matt Reynolds <matt.reynolds@gmail.com>
2012-10-23 18:15:55 -0500

    refactor loading and plotting code
    also updated the plots

--- plots/20121023/makeplot.m
+++ plots/20121023/makeplot.m
@@ -7,39 +7,86 @@ plot_2012_democratic_presidential_primary_tx_harris();
 
 
 %%
+function data = load_data(ci, idx)
+
+% load data from manually processed text file.
+% the file must have one line of space-delimited numeric values per precinct.
+% some election data will be split between multiple files, in that case
+% use the second argument to specify which file to load.
+
+data_dir = ['../../data/' ci.state_fn '/' ci.county_fn '/' ci.date_fn '_' ci.election_fn '_' ci.state_fn '_' ci.county_fn];
+
+suffix = '';
+if nargin > 1
+  suffix = ['_' num2str(idx)];
+end
+
+data_filename = [data_dir '/' ci.date_fn '_' ci.election_fn '_' ci.event_fn '_' ci.state_fn '_' ci.county_fn suffix '.txt'];
+data = load(data_filename);
+
+
+%%
+function plot_filename = plot_cumulative_ballots_vs_vote_share(allvotes, ci)
+
+% 1. Calculate the total number of ballots cast in each precinct.
+totals = sum(allvotes,2);
+
+% 2. Sort precincts from least to most ballots cast.
+[totals_sorted, idx_sorted] = sort(totals);
+allvotes_sorted = allvotes(idx_sorted,:);
+
+% 3. Compute a cumulative sum for the votes cast for each candidate from
+% smallest precincts to largest precincts.  Do the same for the ballot
+% totals.
+cumulativetotals = cumsum(totals_sorted);
+cumulativevotes = cumsum(allvotes_sorted);
+
+% 4. Divide cumulative vote total by cumulative ballot total to get
+% cumulative vote share percentages for each candidate.
+cumulativepercents = bsxfun(@rdivide, cumulativevotes, cumulativetotals);
+
+% 5. Plot cumulative vote share percent vs cumulative ballots cast for each
+% candidate
+close
+figure(1)
+hold on
+for i = 1:size(cumulativepercents,2);
+  plot(cumulativetotals, cumulativepercents(:,i), '.-', 'Color', ci.cmap(i,:))
+end
+title([ci.year ' ' ci.election ' (' ci.event ') - ' ci.county ', ' ci.state ' - Per-Precinct Election Results, Cumulative'])
+xlabel('Cumulative ballots')
+ylabel('Percent vote share')
+legend(ci.candidates,'Location','BestOutside')
+plot_filename = [ci.date_fn '_' ci.election_fn '_' ci.event_fn '_' ci.state_fn '_' ci.county_fn '.png'];
+print('-dpng', '-f1', plot_filename)
+
+
+%%
 function [plot_filename] = plot_2012_republican_presidential_primary_tx_harris()
 
-year = '2012';
-election = 'Republican Primary';
-event = 'President';
-county = 'Harris County';
-state = 'TX';
+ci = struct;
+ci.year = '2012';
+ci.election = 'Republican Primary';
+ci.event = 'President';
+ci.county = 'Harris County';
+ci.state = 'TX';
+ci.date_fn = '20120529';
+ci.election_fn = 'republican_primary';
+ci.event_fn = 'president';
+ci.county_fn = 'harris';
+ci.state_fn = 'tx';
+ci.candidates = {'Paul','Gingrich','Davies','Santorum','Roemer','Huntsman','Romney','Bachmann','Uncommitted'};
+ci.cmap = [ 0 .8 .8; ... % Paul
+            0 .5  0; ... % Gingrich
+            1 .5 .2; ... % Davies
+           .5 .5  0; ... % Santorum
+            0 .5  0; ... % Roemer
+            0  0  1; ... % Huntsman
+            1  0  0; ... % Romney
+            1 .7 .7; ... % Bachmann
+            0  0  0];    % Uncommitted
 
-date_fn = '20120529';
-election_fn = 'republican_primary';
-event_fn = 'president';
-county_fn = 'harris';
-state_fn = 'tx';
-
-plot_filename = [date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.png'];
-
-candidates = {'Paul','Gingrich','Davies','Santorum','Roemer','Huntsman','Romney','Bachmann','Uncommitted'};
-
-cmap = [ 0 .8 .8; ... % Paul
-         0 .5  0; ... % Gingrich
-         1 .5 .2; ... % Davies
-        .5 .5  0; ... % Santorum
-         0 .5  0; ... % Roemer
-         0  0  1; ... % Huntsman
-         1  0  0; ... % Romney
-         1 .7 .7; ... % Bachmann
-         0  0  0];    % Uncommitted
-
-data_dir = ['../../data/' state_fn '/' county_fn '/' date_fn '_' election_fn '_' state_fn '_' county_fn '/'];
-data1_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '_1.txt'];
-data2_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '_2.txt'];
-
-data1 = load(data1_filename);
+data1 = load_data(ci, 1);
 
 % when copying the text from the pdf, it copies the columns in an odd
 % order (note Percent Turnout is between Roemer and Huntsman and Election
@@ -62,7 +109,7 @@ data1 = load(data1_filename);
 
 % there wasn't enough space per page to fit all columns in the original
 % pdf, so the next file is a continuation (starting from p.33 in the pdf)
-data2 = load(data2_filename);
+data2 = load_data(ci, 2);
 
 % 01 Precinct
 % 02 Early Ballots Cast
@@ -73,60 +120,31 @@ data2 = load(data2_filename);
 % 07 Totals
 % 08 Election Ballots Cast
 
-% convert from raw vote totals to percentages
-
 allvotes = [data1(:,[5:9 11:13]) data2(:,6)];
-totals = data2(:,7);
-
-[totals_sorted, idx_sorted] = sort(totals);
-allvotes_sorted = allvotes(idx_sorted,:);
-cumulativetotals = cumsum(totals_sorted);
-cumulativevotes = cumsum(allvotes_sorted);
-cumulativepercents = bsxfun(@rdivide, cumulativevotes, cumulativetotals);
-
-% plot percents against precinct size for each candidate
-
-close
-figure(1)
-hold on
-for i = 1:size(cumulativepercents,2);
-  plot(cumulativetotals, cumulativepercents(:,i), '.-', 'Color', cmap(i,:))
-end
-title([year ' ' election ' (' event ') - ' county ', ' state ' - Per-Precinct Election Results'])
-xlabel('Total ballots in precinct')
-ylabel('Percent vote share')
-legend(candidates,'Location','BestOutside')
-print('-dpng', '-f1', plot_filename)
+plot_filename = plot_cumulative_ballots_vs_vote_share(allvotes, ci);
 
 
 %%
 function [plot_filename] = plot_2012_democratic_presidential_primary_tx_harris()
 
-year = '2012';
-election = 'Democratic Primary';
-event = 'President';
-county = 'Harris County';
-state = 'TX';
+ci = struct;
+ci.year = '2012';
+ci.election = 'Democratic Primary';
+ci.event = 'President';
+ci.county = 'Harris County';
+ci.state = 'TX';
+ci.date_fn = '20120529';
+ci.election_fn = 'democratic_primary';
+ci.event_fn = 'president';
+ci.county_fn = 'harris';
+ci.state_fn = 'tx';
+ci.candidates = {'Wolfe', 'Ely', 'Obama', 'Richardson'};
+ci.cmap = [ 1  0  0; ... % Wolfe
+            0 .8  0; ... % Ely
+            0  0  1; ... % Obama
+           .5  0 .5];    % Richardson
 
-date_fn = '20120529';
-election_fn = 'democratic_primary';
-event_fn = 'president';
-county_fn = 'harris';
-state_fn = 'tx';
-
-plot_filename = [date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.png'];
-
-candidates = {'Wolfe', 'Ely', 'Obama', 'Richardson'};
-
-cmap = [ 1  0  0; ... % Wolfe
-         0 .8  0; ... % Ely
-         0  0  1; ... % Obama
-        .5  0 .5];    % Richardson
-
-data_dir = ['../../data/' state_fn '/' county_fn '/' date_fn '_' election_fn '_' state_fn '_' county_fn '/'];
-data_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.txt'];
-
-data = load(data_filename);
+data = load_data(ci);
 
 % 01 Precinct
 % 02 Early Ballots Cast
@@ -141,64 +159,37 @@ data = load(data_filename);
 % 11 Election Ballots Cast
 
 allvotes = data(:,5:8);
-totals = data(:,9);
+plot_filename = plot_cumulative_ballots_vs_vote_share(allvotes, ci);
 
-[totals_sorted, idx_sorted] = sort(totals);
-allvotes_sorted = allvotes(idx_sorted,:);
-cumulativetotals = cumsum(totals_sorted);
-cumulativevotes = cumsum(allvotes_sorted);
-cumulativepercents = bsxfun(@rdivide, cumulativevotes, cumulativetotals);
-
-% plot percents against precinct size for each candidate
-
-close
-figure(1)
-hold on
-for i = 1:size(cumulativepercents,2);
-  plot(cumulativetotals, cumulativepercents(:,i), '.-', 'Color', cmap(i,:))
-end
-title([year ' ' election ' (' event ') - ' county ', ' state ' - Per-Precinct Election Results'])
-xlabel('Total ballots in precinct')
-ylabel('Percent vote share')
-legend(candidates,'Location','BestOutside')
-print('-dpng', '-f1', plot_filename)
 
 %%
 function [plot_filename] = plot_2008_republican_presidential_primary_tx_harris()
 
-year = '2008';
-election = 'Republican Primary';
-event = 'President';
-county = 'Harris County';
-state = 'TX';
+ci = struct;
+ci.year = '2008';
+ci.election = 'Republican Primary';
+ci.event = 'President';
+ci.county = 'Harris County';
+ci.state = 'TX';
+ci.date_fn = '20080304';
+ci.election_fn = 'republican_primary';
+ci.event_fn = 'president';
+ci.county_fn = 'harris';
+ci.state_fn = 'tx';
+ci.candidates = {'Hunter','Thompson','Cort','McCain','Paul','Giuliani','Tran','Huckabee','Keyes','Romney','Uncommitted'};
+ci.cmap = [ 0 .5  0; ... % Hunter
+            0  0  0; ... % Thompson
+            1 .5 .2; ... % Cort
+            1  0  0; ... % McCain
+            0 .8 .8; ... % Paul
+           .7  1 .7; ... % Giuliani
+            1 .7 .7; ... % Tran
+            0  0  1; ... % Huckabee
+           .7 .7  1; ... % Keyes
+           .5  0  0; ... % Romney
+            0  0  0];    % Uncommitted
 
-date_fn = '20080304';
-election_fn = 'republican_primary';
-event_fn = 'president';
-county_fn = 'harris';
-state_fn = 'tx';
-
-plot_filename = [date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.png'];
-
-candidates = {'Hunter','Thompson','Cort','McCain','Paul','Giuliani','Tran','Huckabee','Keyes','Romney','Uncommitted'};
-
-cmap = [ 0 .5  0; ... % Hunter
-         0  0  0; ... % Thompson
-         1 .5 .2; ... % Cort
-         1  0  0; ... % McCain
-         0 .8 .8; ... % Paul
-        .7  1 .7; ... % Giuliani
-         1 .7 .7; ... % Tran
-         0  0  1; ... % Huckabee
-        .7 .7  1; ... % Keyes
-        .5  0  0; ... % Romney
-         0  0  0];    % Uncommitted
-
-data_dir = ['../../data/' state_fn '/' county_fn '/' date_fn '_' election_fn '_' state_fn '_' county_fn '/'];
-data1_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '_1.txt'];
-data2_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '_2.txt'];
-
-data1 = load(data1_filename);
+data1 = load_data(ci, 1);
 
 % 01 Precinct
 % 02 Early Voting Ballots Cast
@@ -215,7 +206,7 @@ data1 = load(data1_filename);
 % 13 Mike Huckabee
 % 14 Election Day Ballots Cast
 
-data2 = load(data2_filename);
+data2 = load_data(ci, 2);
 
 % 01 Precinct
 % 02 Early Voting Ballots Cast
@@ -229,59 +220,32 @@ data2 = load(data2_filename);
 % 10 Election Day Ballots Cast
 
 allvotes = [data1(:,[5:9 11:13]) data2(:,6:8)];
-totals = data2(:,9);
-
-[totals_sorted, idx_sorted] = sort(totals);
-allvotes_sorted = allvotes(idx_sorted,:);
-cumulativetotals = cumsum(totals_sorted);
-cumulativevotes = cumsum(allvotes_sorted);
-cumulativepercents = bsxfun(@rdivide, cumulativevotes, cumulativetotals);
-
-% plot percents against precinct size for each candidate
-
-close
-figure(1)
-hold on
-for i = 1:size(cumulativepercents,2);
-  plot(cumulativetotals, cumulativepercents(:,i), '.-', 'Color', cmap(i,:))
-end
-title([year ' ' election ' (' event ') - ' county ', ' state ' - Per-Precinct Election Results'])
-xlabel('Total ballots in precinct')
-ylabel('Percent vote share')
-legend(candidates,'Location','BestOutside')
-print('-dpng', '-f1', plot_filename)
+plot_filename = plot_cumulative_ballots_vs_vote_share(allvotes, ci);
 
 
 %%
 function [plot_filename] = plot_2008_democratic_presidential_primary_tx_harris()
 
-year = '2008';
-election = 'Democratic Primary';
-event = 'President';
-county = 'Harris County';
-state = 'TX';
+ci = struct;
+ci.year = '2008';
+ci.election = 'Democratic Primary';
+ci.event = 'President';
+ci.county = 'Harris County';
+ci.state = 'TX';
+ci.date_fn = '20080304';
+ci.election_fn = 'democratic_primary';
+ci.event_fn = 'president';
+ci.county_fn = 'harris';
+ci.state_fn = 'tx';
+ci.candidates = {'Obama','Dodd','Clinton','Biden','Richardson','Edwards'};
+ci.cmap = [ 0  0  1; ... % Obama
+            1 .7 .7; ... % Dodd
+            1  0  0; ... % Clinton
+           .7 .7  1; ... % Biden
+           .5  0 .5; ... % Richardson
+           .7  1 .7];    % Edwards
 
-date_fn = '20080304';
-election_fn = 'democratic_primary';
-event_fn = 'president';
-county_fn = 'harris';
-state_fn = 'tx';
-
-plot_filename = [date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.png'];
-
-candidates = {'Obama','Dodd','Clinton','Biden','Richardson','Edwards'};
-
-cmap = [ 0  0  1; ... % Obama
-         1 .7 .7; ... % Dodd
-         1  0  0; ... % Clinton
-        .7 .7  1; ... % Biden
-        .5  0 .5; ... % Richardson
-        .7  1 .7];    % Edwards
-
-data_dir = ['../../data/' state_fn '/' county_fn '/' date_fn '_' election_fn '_' state_fn '_' county_fn '/'];
-data_filename = [data_dir '/' date_fn '_' election_fn '_' event_fn '_' state_fn '_' county_fn '.txt'];
-
-data = load(data_filename);
+data = load_data(ci);
 
 % 01 Precinct
 % 02 Early Ballots Cast
@@ -298,24 +262,4 @@ data = load(data_filename);
 % 13 Election Ballots Cast
 
 allvotes = data(:,[5:9 11]);
-totals = data(:,12);
-
-[totals_sorted, idx_sorted] = sort(totals);
-allvotes_sorted = allvotes(idx_sorted,:);
-cumulativetotals = cumsum(totals_sorted);
-cumulativevotes = cumsum(allvotes_sorted);
-cumulativepercents = bsxfun(@rdivide, cumulativevotes, cumulativetotals);
-
-% plot percents against precinct size for each candidate
-
-close
-figure(1)
-hold on
-for i = 1:size(cumulativepercents,2);
-  plot(cumulativetotals, cumulativepercents(:,i), '.-', 'Color', cmap(i,:))
-end
-title([year ' ' election ' (' event ') - ' county ', ' state ' - Per-Precinct Election Results'])
-xlabel('Total ballots in precinct')
-ylabel('Percent vote share')
-legend(candidates,'Location','BestOutside')
-print('-dpng', '-f1', plot_filename)
+plot_filename = plot_cumulative_ballots_vs_vote_share(allvotes, ci);
